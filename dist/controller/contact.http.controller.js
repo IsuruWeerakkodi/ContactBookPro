38536a10466f7fefe0499d9af738742195898265
Isuru Weerakkodi <isuruweerakkodi24@gmail.com>
2024-02-11 17:06:35 +0530

    add business logics of controller

--- dist/controller/contact.http.controller.js
+++ dist/controller/contact.http.controller.js
@@ -9,7 +9,7 @@ const promise_1 = __importDefault(require("mysql2/promise"));
 const controller = (0, express_1.Router)();
 exports.TaskHttpController = controller;
 controller.get('/', getAllContacts);
-controller.get('/:id', getContact);
+controller.get('/:id', getContactByName);
 controller.post('/', saveContact);
 controller.patch('/:id', updateContact);
 controller.delete('./:id', deleteContact);
@@ -21,14 +21,55 @@ const pool = promise_1.default.createPool({
     password: 'mysql',
     connectionLimit: 10
 });
-function getAllContacts() {
+async function getAllContacts(req, res) {
+    const connection = await pool.getConnection();
+    const [contactlist] = await connection.execute('SELECT * FROM contacts');
+    res.json(contactlist);
+    pool.releaseConnection(connection);
 }
-function getContact() {
+async function getContactByName(req, res) {
+    if (!req.query.name)
+        res.sendStatus(400);
+    const connection = await pool.getConnection();
+    const [contact] = await connection.execute('SELECT * FROM contacts WHERE name = ?', req.query.name);
+    res.json(contact);
+    pool.releaseConnection(connection);
 }
-function saveContact() {
+async function saveContact(req, res) {
+    const contact = req.body;
+    const connection = await pool.getConnection();
+    const [{ insertId }] = await connection.execute('INSERT INTO contacts(id, name, description, phone, email) VALUES (?, ?, ?, ?, ?)', [contact.id, contact.name, contact.description, contact.phone, contact.email]);
+    pool.releaseConnection(connection);
+    contact.id = insertId;
+    res.status(201).json(contact);
 }
-function updateContact() {
+async function updateContact(req, res) {
+    const contactId = +req.params.id;
+    const contact = req.body;
+    const connection = await pool.getConnection();
+    const result = await connection.execute('SELECT * FROM contacts WHERE id = ?', [contactId]);
+    if (!result.length) {
+        res.sendStatus(404);
+        return;
+    }
+    else {
+        await connection.execute('UPDATE contacts SET name = ?, description = ?, phone = ?, email = ?', [contact.name, contact.description, contact.phone, contact.email]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
 }
-function deleteContact() {
+async function deleteContact(req, res) {
+    const contactId = +req.params.id;
+    const contact = req.body;
+    const connection = await pool.getConnection();
+    const result = await connection.execute('SELECT * FROM contacts WHERE id = ?', [contactId]);
+    if (!result.length) {
+        res.sendStatus(404);
+        return;
+    }
+    else {
+        await connection.execute('DELETE FROM contacts WHERE id = ?', [contactId]);
+    }
+    pool.releaseConnection(connection);
 }
 //# sourceMappingURL=contact.http.controller.js.map
--- src/controller/contact.http.controller.ts
+++ src/controller/contact.http.controller.ts
@@ -1,12 +1,12 @@
 import {Router} from "express";
 import {Request, Response} from "express-serve-static-core";
-import mysql, {ResultSetHeader} from "mysql2/promise";
+import mysql, {ResultSetHeader} from 'mysql2/promise';
 import {ContactTO} from "../to/contact.to";
 
 const controller = Router();
 
 controller.get('/', getAllContacts);
-controller.get('/:id', getContact);
+controller.get('/:id', getContactByName);
 controller.post('/', saveContact);
 controller.patch('/:id', updateContact);
 controller.delete('./:id', deleteContact)
@@ -21,16 +21,56 @@ const pool = mysql.createPool({
     connectionLimit:10
 })
 
-function getAllContacts() {
+async function getAllContacts(req:Request, res:Response) {
+    const connection = await pool.getConnection();
+    const [contactlist] = await connection.execute('SELECT * FROM contacts');
+    res.json(contactlist);
+    pool.releaseConnection(connection);
 }
-function getContact() {
-
+async function getContactByName(req:Request, res:Response) {
+    if (!req.query.name) res.sendStatus(400)
+    const connection = await pool.getConnection();
+    const [contact] = await connection.execute('SELECT * FROM contacts WHERE name = ?', req.query.name);
+    res.json(contact);
+    pool.releaseConnection(connection);
 }
-function saveContact() {
-
+async function saveContact(req:Request, res:Response) {
+    const contact = <ContactTO> req.body;
+    const connection = await pool.getConnection();
+    const [{insertId}] = await connection.execute<ResultSetHeader>
+        ('INSERT INTO contacts(id, name, description, phone, email) VALUES (?, ?, ?, ?, ?)',
+            [contact.id, contact.name, contact.description, contact.phone, contact.email]);
+    pool.releaseConnection(connection);
+    contact.id = insertId;
+    res.status(201).json(contact);
 }
-function updateContact() {
-
+async function updateContact(req:Request, res:Response) {
+    const contactId = +req.params.id;
+    const contact =<ContactTO> req.body;
+    const connection =await pool.getConnection();
+    const result = await connection.execute('SELECT * FROM contacts WHERE id = ?', [contactId]);
+    if (!result.length){
+        res.sendStatus(404);
+        return;
+    }
+    else{
+        await connection.execute
+            ('UPDATE contacts SET name = ?, description = ?, phone = ?, email = ?',
+                [contact.name, contact.description, contact.phone, contact.email]);
+        res.sendStatus(204);
+    }
+    pool.releaseConnection(connection);
 }
-function deleteContact() {
+async function deleteContact(req:Request, res:Response) {
+    const contactId = +req.params.id;
+    const contact =<ContactTO> req.body;
+    const connection = await pool.getConnection();
+    const result = await connection.execute('SELECT * FROM contacts WHERE id = ?', [contactId]);
+    if (!result.length){
+        res.sendStatus(404);
+        return;
+    }else {
+        await connection.execute('DELETE FROM contacts WHERE id = ?', [contactId]);
+    }
+    pool.releaseConnection(connection);
 }
