f0d3d483387f339da6d61f2fd49df0b07f793b1b
Isuru Weerakkodi <isuruweerakkodi24@gmail.com>
2024-02-17 00:35:25 +0530

    add ws controller

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import express, {json, urlencoded} from 'express';
+import expressWs from "express-ws";
 import {ContactHttpController} from "./controller/contact.http.controller.js"
+import {ContactWsController} from "./ws/contact.ws.controller.js"
 import cors from 'cors';
 
-const app = express();
+const app = expressWs(express()).app;
+
 app.use(json());
 app.use(urlencoded({extended:true}));
 app.use(cors());
 
 app.use('/api/v1/contact', ContactHttpController)
+app.ws('/api/v1/contact', ContactWsController)
 
 app.listen(8080, ()=> console.log("Server is listening to 8080"))
 
---
+++ src/ws/contact.ws.controller.ts
@@ -0,0 +1,6 @@
+import {Request} from "express";
+import {WebSocket} from 'ws';
+
+export function ContactWsController(ws: WebSocket, req: Request) {
+
+}
