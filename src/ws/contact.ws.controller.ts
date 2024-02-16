3d0e9632b4988d0108e68e2f2250b242d7deff75
Isuru Weerakkodi <isuruweerakkodi24@gmail.com>
2024-02-17 00:42:38 +0530

    complete establishing websocket

--- src/ws/contact.ws.controller.ts
+++ src/ws/contact.ws.controller.ts
@@ -2,5 +2,22 @@ import {Request} from "express";
 import {WebSocket} from 'ws';
 
 export function ContactWsController(ws: WebSocket, req: Request) {
+    // New Web Socket Connection
+    console.log("New websocket connection established")
+
+    // Disconnect
+    ws.on('close', () => console.log("Web socket connection closed"))
+
+    // Receive
+    ws.on('message', msg => {
+        console.log(msg)
+
+        //Send
+        ws.send(`Server: ${msg}`);
+    })
+
+
+    // Send
+
 
 }
